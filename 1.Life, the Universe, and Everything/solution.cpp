2d573fdffe7af8eb5cd3d938b20e667b37b50a8b
advecchia <advecchia@inf.ufrgs.br>
2020-02-01 11:57:54 -0300

    Solution for the Universe answer solved.

--- 1.Life, the Universe, and Everything/solution.cpp
+++ 1.Life, the Universe, and Everything/solution.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+const int ANSWER_OF_THE_LIFE=42;
+
+/**
+ * Will read from input until receive the answer of the life, the universe and everything.
+ */
 int main() {
-    // your code goes here
-    cout << "Hello, World!\n";
-    return 0;
+    int input;
+    while(cin >> input) {
+        if (ANSWER_OF_THE_LIFE == input) {
+            return 0;
+        }
+        cout << input;
+        cout << "\n";
+    }
 }
